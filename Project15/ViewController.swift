ff0c615bc208134733f41d716f2846c64ac0ccfa
jslusser <jslusser@gmail.com>
2017-06-16 10:10:21 +0300

    Prepare for action

--- Project15/ViewController.swift
+++ Project15/ViewController.swift
@@ -9,9 +9,26 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBAction func tapped(_ sender: UIButton) {
+        currentAnimation += 1
+        
+        if currentAnimation > 7 {
+            currentAnimation = 0
+        }
+    }
 
+    @IBOutlet weak var tap: UIButton!
+    
+    var imageView: UIImageView!
+    var currentAnimation = 0
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        imageView = UIImageView(image: UIImage(named: "penguin"))
+        imageView.center = CGPoint(x: 512, y: 384)
+        view.addSubview(imageView)
         // Do any additional setup after loading the view, typically from a nib.
     }
 
